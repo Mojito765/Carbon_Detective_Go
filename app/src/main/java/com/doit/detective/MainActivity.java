08459cafe4e6b2a58855d8959ebe391b2f33ea61
Mojito765 <nicknick765@gmail.com>
2022-12-24 00:53:30 +0800

    Test: Home fragment - card view
    issue #018

--- app/src/main/java/com/doit/detective/MainActivity.java
+++ app/src/main/java/com/doit/detective/MainActivity.java
@@ -26,10 +26,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     ChipNavigationBar chipNavigationBar;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
 //toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
--- app/src/main/java/com/doit/detective/fragment/advise_fragment.java
+++ app/src/main/java/com/doit/detective/fragment/advise_fragment.java
@@ -9,17 +9,16 @@ import androidx.fragment.app.Fragment;
 
 import com.doit.detective.R;
 
-public class home_fragment extends Fragment {
+public class advise_fragment extends Fragment {
 
-
-    public home_fragment() {
+    public advise_fragment() {
         // Required empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.home_fragment, container, false);
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.advise_fragment, container, false);
     }
 }
