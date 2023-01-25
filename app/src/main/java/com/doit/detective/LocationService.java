d399b38a5f35066382d6e7f282f4dbc9b1f4432b
Mojito765 <nicknick765@gmail.com>
2023-01-25 17:11:15 +0800

    Test: LocationService - running in the background
    issue #037

---
+++ app/src/main/java/com/doit/detective/LocationService.java
@@ -0,0 +1,165 @@
+package com.doit.detective;
+
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Build;
+import android.os.IBinder;
+import android.os.Looper;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
+
+public class LocationService extends Service {
+
+   public static ArrayList<LatLng> locationArrayList = new ArrayList<>();
+
+    FusedLocationProviderClient fusedLocationClient;
+    LocationRequest locationRequest;
+    LocationCallback locationCallback;
+
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest,
+                locationCallback,
+                Looper.getMainLooper());
+    }
+
+    protected void createLocationRequest() {
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(3000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        new Notification();
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) createNotificationChanel() ;
+        else startForeground(
+                1,
+                new Notification()
+        );
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(3000);
+        locationRequest.setFastestInterval(3000);
+        locationRequest.setMaxWaitTime(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                Location location =  locationResult.getLastLocation();
+                assert location != null;
+                Toast.makeText(getApplicationContext(),
+                      "Lat: "+ location.getLatitude() + '\n' +
+                        "Long: " + location.getLongitude(), Toast.LENGTH_LONG).show();
+
+                //locationArrayList.add(new LatLng(location.getLatitude(), location.getLongitude()));
+
+               /* for (Location location : locationResult.getLocations()) {
+                  location
+                }*/
+            }
+        };
+        startLocationUpdates();
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void createNotificationChanel() {
+        String notificationChannelId = "Location channel id";
+        String channelName = "Background Service";
+
+        NotificationChannel chan = new NotificationChannel(
+                notificationChannelId,
+                channelName,
+                NotificationManager.IMPORTANCE_NONE
+        );
+        chan.setLightColor(Color.BLUE);
+        chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+        NotificationManager manager = getSystemService(NotificationManager.class);
+
+        manager.createNotificationChannel(chan);
+
+        NotificationCompat.Builder notificationBuilder =
+               new NotificationCompat.Builder(this, notificationChannelId);
+
+        Notification notification = notificationBuilder.setOngoing(true)
+                .setContentTitle("Location updates:")
+                .setPriority(NotificationManager.IMPORTANCE_MIN)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .build();
+        startForeground(2, notification);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        fusedLocationClient.removeLocationUpdates(locationCallback);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    public double GetDistanceFromLatLonInKm(double lat1, double lon1, double lat2, double lon2)
+    {
+        final int R = 6371;
+        // Radius of the earth in km
+        double dLat = deg2rad(lat2 - lat1);
+        // deg2rad below
+        double dLon = deg2rad(lon2 - lon1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double d = R * c;
+        // Distance in km
+        return d;
+    }
+    private double deg2rad(double deg)
+    {
+        return deg * (Math.PI / 180);
+    }
+}
--- app/src/main/java/com/doit/detective/go_activity.java
+++ app/src/main/java/com/doit/detective/go_activity.java
@@ -1,22 +1,164 @@
 package com.doit.detective;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.doit.detective.fragment.dialog7_fragment;
+import com.google.android.material.button.MaterialButtonToggleGroup;
+
+import java.io.File;
+import java.io.FileWriter;
 
 public class go_activity extends AppCompatActivity {
+
+    private static int MY_FINE_LOCATION_REQUEST = 99;
+    private static int MY_BACKGROUND_LOCATION_REQUEST = 100;
+
+    LocationService mLocationService = new LocationService();
+    Intent mServiceIntent;
+
+    Button startServiceBtn, stopServiceBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_go);
+        
+        btnStop();
 
+        topBack();
+
+        topQA();
+
+        btnTransportation();
+
+        startServiceBtn = findViewById(R.id.start_service_btn);
+        stopServiceBtn = findViewById(R.id.stop_service_btn);
+
+        startServiceBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (ActivityCompat.checkSelfPermission(go_activity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+
+                        if (ActivityCompat.checkSelfPermission(go_activity.this, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                                != PackageManager.PERMISSION_GRANTED) {
+
+
+                            AlertDialog alertDialog = new AlertDialog.Builder(go_activity.this).create();
+                            alertDialog.setTitle("Background permission");
+                            alertDialog.setMessage(getString(R.string.background_location_permission_message));
+
+                            alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Start service anyway",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            starServiceFunc();
+                                            dialog.dismiss();
+                                        }
+                                    });
+
+                            alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Grant background Permission",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            requestBackgroundLocationPermission();
+                                            dialog.dismiss();
+                                        }
+                                    });
+
+                            alertDialog.show();
+
+
+                        }else if (ActivityCompat.checkSelfPermission(go_activity.this, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                                == PackageManager.PERMISSION_GRANTED){
+                            starServiceFunc();
+                        }
+                    }else{
+                        starServiceFunc();
+                    }
+
+                }else if (ActivityCompat.checkSelfPermission(go_activity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED){
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(go_activity.this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+
+                        AlertDialog alertDialog = new AlertDialog.Builder(go_activity.this).create();
+                        alertDialog.setTitle("ACCESS_FINE_LOCATION");
+                        alertDialog.setMessage("Location permission required");
+
+                        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        requestFineLocationPermission();
+                                        dialog.dismiss();
+                                    }
+                                });
+
+
+                        alertDialog.show();
+
+                    } else {
+                        requestFineLocationPermission();
+                    }
+                }
+
+            }
+        });
+
+        stopServiceBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopServiceFunc();
+            }
+        });
+
+
+    }
+//01/24
+    private void btnTransportation() {
+        MaterialButtonToggleGroup materialButtonToggleGroup =
+                findViewById(R.id.toggleButton);
+//        int buttonId = materialButtonToggleGroup.getCheckedButtonId();
+//        MaterialButton button = materialButtonToggleGroup.findViewById(buttonId);
+
+        materialButtonToggleGroup.addOnButtonCheckedListener(new MaterialButtonToggleGroup.OnButtonCheckedListener() {
+            @Override
+            public void onButtonChecked(MaterialButtonToggleGroup group, int checkedId, boolean isChecked) {
+                if (isChecked) {
+                    if (checkedId == R.id.trans1) {
+                        Toast.makeText(go_activity.this, "Button1 Clicked", Toast.LENGTH_SHORT).show();
+                    }
+                    else if (checkedId == R.id.trans2) {
+                        Toast.makeText(go_activity.this, "Button2 Clicked", Toast.LENGTH_SHORT).show();
+                    }
+                    else if (checkedId == R.id.trans3) {
+                        Toast.makeText(go_activity.this, "Button3 Clicked", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+    private void btnStop() {
         Button btnIntentActivityHome = findViewById(R.id.btn_stop);
         btnIntentActivityHome.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,15 +168,9 @@ public class go_activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
 
-        TextView goTextView=findViewById(R.id.back);
-        goTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
+    private void topQA() {
         TextView questionTextView=findViewById(R.id.question);
         questionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,4 +180,111 @@ public class go_activity extends AppCompatActivity {
             }
         });
     }
+
+    private void topBack() {
+        TextView backTextView=findViewById(R.id.back);
+        backTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        Toast.makeText(this, Integer.toString(requestCode), Toast.LENGTH_LONG).show();
+
+        if ( requestCode == MY_FINE_LOCATION_REQUEST){
+
+            if (grantResults.length !=0 /*grantResults.isNotEmpty()*/ && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                        requestBackgroundLocationPermission();
+                    }
+                }
+
+            } else {
+                Toast.makeText(this, "ACCESS_FINE_LOCATION permission denied", Toast.LENGTH_LONG).show();
+                if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                 /*   startActivity(
+                            Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                            Uri.fromParts("package", this.getPackageName(), null),),);*/
+
+                    startActivity(new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                            Uri.parse("package:com.trickyworld.locationupdates")
+                    ));
+
+                }
+            }
+
+        }else if (requestCode == MY_BACKGROUND_LOCATION_REQUEST){
+
+            if (grantResults.length!=0 /*grantResults.isNotEmpty()*/ && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Background location Permission Granted", Toast.LENGTH_LONG).show();
+                }
+            } else {
+                Toast.makeText(this, "Background location permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+    private void starServiceFunc(){
+        mLocationService = new LocationService();
+        mServiceIntent = new Intent(this, mLocationService.getClass());
+        if (!Util.isMyServiceRunning(mLocationService.getClass(), this)) {
+            startService(mServiceIntent);
+            Toast.makeText(this, getString(R.string.service_start_successfully), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, getString(R.string.service_already_running), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void stopServiceFunc(){
+        mLocationService = new LocationService();
+        mServiceIntent = new Intent(this, mLocationService.getClass());
+        if (Util.isMyServiceRunning(mLocationService.getClass(), this)) {
+            stopService(mServiceIntent);
+            Toast.makeText(this, "Service stopped!!", Toast.LENGTH_SHORT).show();
+            //saveLocation(); // explore it by your self
+        } else {
+            Toast.makeText(this, "Service is already stopped!!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    private void requestBackgroundLocationPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION},
+                MY_BACKGROUND_LOCATION_REQUEST);
+    }
+
+    private void requestFineLocationPermission() {
+        ActivityCompat.requestPermissions(this,  new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, MY_FINE_LOCATION_REQUEST);
+    }
+
+    public void saveLocation(){
+        File dir = new File(this.getFilesDir(), "trickyworld");
+        if(!dir.exists()){
+            dir.mkdir();
+        }
+
+        try {
+            File userLocation = new File(dir, "userlocation.txt");
+            FileWriter writer = new FileWriter(userLocation);
+            writer.append(LocationService.locationArrayList.toString());
+            writer.flush();
+            writer.close();
+            LocationService.locationArrayList.clear();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 }
