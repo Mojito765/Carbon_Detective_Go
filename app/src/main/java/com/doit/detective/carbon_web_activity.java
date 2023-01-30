75c17b5ff4adf29260bd8efa212004a27ea1146b
Mojito765 <nicknick765@gmail.com>
2023-01-31 05:34:22 +0800

    Feat: Web page top bar - add
    issue #055

--- app/src/main/java/com/doit/detective/carbon_web_activity.java
+++ app/src/main/java/com/doit/detective/carbon_web_activity.java
@@ -6,12 +6,15 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class carbon_web_activity extends AppCompatActivity {
     WebView webview;
     WebSettings webSettings;
     String url="https://cfp-calculate.tw/cfpc/Carbon/WebPage/InstitutionDesc.aspx";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +25,16 @@ public class carbon_web_activity extends AppCompatActivity {
         webSettings.setJavaScriptEnabled(true);//開啟javascript功能
         webview.setWebViewClient(new WebViewClient());//新增瀏覽器客戶端
         webview.loadUrl(url);//讀取url網站
+
+        Toolbar myChildToolbar = findViewById(R.id.topAppBar);
+        setSupportActionBar(myChildToolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        assert ab != null;
+        ab.setDisplayHomeAsUpEnabled(true);
     }
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
